a7cef719d6a8523a2b5d7b9fb282b574d62783c5
LexaWhat <udiejfkskfid@gmail.com>
2025-04-04 21:59:33 +0500

    Base Comfort

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{self, Write};
+
+fn get_string() -> String {
+    io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    input.trim().to_string()
+}
+
+fn get_int() -> i32 {
+    loop {
+        let mut input = String::new();
+        io::stdout().flush().unwrap();
+
+        io::stdin().read_line(&mut input).unwrap();
+
+        match input.trim().parse::<i32>() {
+            Ok(num) => return num,
+            Err(_) => println!("Retry: Please enter a valid number."),
+        }
+    }
+}
+
 fn main() {
     let name: &str = "Alex";
     println!("{}", char_dis(name, 2));
