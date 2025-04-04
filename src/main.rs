2924a0574cc38996aaa381e516f2aabd97278efc
LexaWhat <udiejfkskfid@gmail.com>
2025-04-04 21:58:37 +0500

    Character Display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let name: &str = "Alex";
+    println!("{}", char_dis(name, 2));
+}
+
+fn char_dis(x: &str, y: usize) -> char {
+    x.chars().nth(y).unwrap_or('_')
 }
